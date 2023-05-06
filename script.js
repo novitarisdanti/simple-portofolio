01c0c868ebe02b8862b48b09201f42d25e67033d
novitarisdanti <novitarise@gmail.com>
2023-05-06 12:09:46 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function button(){
+    alert("Thank you for Visiting, Enjoy your journey!")
+}
+
+var content = document.getElementsByTagName('body')[0];
+var darkMode = document.getElementById('dark-mode');
+darkMode.addEventListener('click', function(){
+    darkMode.classList.toggle('active');
+    content.classList.toggle('night')
+})
